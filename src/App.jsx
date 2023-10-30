5d8ef70df8d0f993a7a87bca9cbbf2ebe84a2d92
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-30 16:03:40 -0500

    setting up navbar routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 const App = () => {
     return(
         <BrowserRouter>
             <Navbar />
             <Routes>
-                <Route path="/">Go Home</Route>
-                <Route path="/films">View Films</Route>
-                <Route path="/people">View People</Route>
+                <Route path="/" element={<Home />}>Go Home</Route>
+                {/* <Route path="/films" element={<Films />}>View Films</Route> */}
+                {/* <Route path="/people" element={<People />}>View People</Route> */}
             </Routes>
         </BrowserRouter>
     );
