229268c77fcaaa795a75214f7bf648c72e9583f4
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-31 15:18:24 -0500

    styled people cards

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -7,20 +7,36 @@ const People = () => {
     const [people, setPeople] = useState([]);
     const getPeople = () => {
         fetch('http://api-ghibli.herokuapp.com/people')
-        .then(response => response.json())
-        .then(allPeople => setPeople(allPeople))
+            .then(response => response.json())
+            .then(allPeople => setPeople(allPeople))
     };
 
     useEffect(() => {
         getPeople();
-    },[people]);
+    }, [people]);
 
 
     return (
-        <div className="mt-2 text-center">
-            <h1>People</h1>
-        </div>
-        
+        <>
+            {/* load different characters as cards */}
+            <div className="container">
+                <section className="mt-5">
+                    {people.map(character => (
+                        <div key={character.id}>
+                            <div className="card" style={{ width: "20rem", width: "auto" }}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{character.name}</h5>
+                                    <Link to={`/people/${character.id}`} className="btn btn-outline-secondary">
+                                        More Details
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </section>
+
+            </div>
+        </>
     );
 }
 
