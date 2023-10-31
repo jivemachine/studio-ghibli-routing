25f0fa88f171d8f86727bbbdaa4e171b14bb9a8c
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-31 14:40:15 -0500

    added more styling and added studio ghibli logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="nav nav-pills justify-content-center p-2 shadow">
-            <NavLink to="/" className="nav-link">Go Home</NavLink>
+        // <nav className="nav nav-pills justify-content-left p-2 shadow">
+        <nav className="nav shadow text-muted">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1920px-Studio_Ghibli_logo.svg.png" style={{height: '7rem', width: 'auto'}} alt="" />
+            <NavLink to="/" className={({ isActive }) => `nav-link ${isActive && 'active'} nav-link ms-auto`}>Go Home</NavLink>
             <NavLink to="/films" className="nav-link">View Films</NavLink>
             <NavLink to="/people" className="nav-link">View People</NavLink>
         </nav>
