4a53e6c79fa43174b613da8840f3ae09314ae488
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-31 14:34:26 -0500

    'finished' home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 const Home = () => {
     return (
-        <div className="mt-5">
-            <h1>Home</h1>
+        // <div className="mt-5 text-center">
+        //     <h1>Home</h1>
+        // </div>
+        <div className="container justify-content-center mt-5" style={{ height: "10rem", width: "50rem" }}>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title text-center">Studio Ghibli Wiki-Style Project</h5>
+                    <p className="card-text">
+                        My main goal with this application is creating a little interface to quickly reach different data using the <a href="https://ghibliapi.vercel.app/" target="_blank">Studio Ghibli API</a>. All of the data used in this project comes from that site.</p>
+                    <p>I coded this project using react and styled it all in bootstrap 5. The GitHub repo for the project is <a href="https://github.com/jivemachine/studio-ghibli-routing" target="_blank">here</a>.
+                        This project is a coding lab from the <a href="https://covalence.io" target="_blank">covalence</a> react course.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
