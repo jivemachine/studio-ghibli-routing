8c50604fe940c51e449a87ce4479ebd146fd1db4
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-31 14:59:53 -0500

    fetched data & set data

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const People = () => {
+
+    // getting all people in films using API
+    const [people, setPeople] = useState([]);
+    const getPeople = () => {
+        fetch('http://api-ghibli.herokuapp.com/people')
+        .then(response => response.json())
+        .then(allPeople => setPeople(allPeople))
+    };
+
+    useEffect(() => {
+        getPeople();
+    },[people]);
+
+
     return (
-        <div className="mt-5">
+        <div className="mt-2 text-center">
             <h1>People</h1>
         </div>
+        
     );
 }
 
