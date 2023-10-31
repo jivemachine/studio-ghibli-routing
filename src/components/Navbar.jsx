cfd6b260dcd635f4785ea1e6a8db63dd8ab83787
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-31 15:48:51 -0500

    removed commented out code added new route for vehicles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        // <nav className="nav nav-pills justify-content-left p-2 shadow">
         <nav className="nav shadow text-muted">
             <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1920px-Studio_Ghibli_logo.svg.png" style={{height: '7rem', width: 'auto'}} alt="" />
             <NavLink to="/" className={({ isActive }) => `nav-link ${isActive && 'active'} nav-link ms-auto`}>Go Home</NavLink>
             <NavLink to="/films" className="nav-link">View Films</NavLink>
             <NavLink to="/people" className="nav-link">View People</NavLink>
+            <NavLink to="/vehicles" className="nav-link">View Vehicles</NavLink>
         </nav>
     );
 }
