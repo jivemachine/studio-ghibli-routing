26d4390cf3c21c7c47dce6a69d44f4402d08ea95
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-31 14:54:41 -0500

    Added producer and film description & added more bootstrap styling

--- src/pages/FilmDetails.jsx
+++ src/pages/FilmDetails.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link,useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const FilmDetails = () => {
 
@@ -9,22 +9,31 @@ const FilmDetails = () => {
 
     useEffect(() => {
         fetch(`http://api-ghibli.herokuapp.com/films/${filmid}`)
-        .then(response => response.json())
-        .then(filmData => setFilmDetails(filmData))
-        .catch(event => alert(event.message));
+            .then(response => response.json())
+            .then(filmData => setFilmDetails(filmData))
+            .catch(event => alert(event.message));
     }, [filmid]);
 
     return (
-        <div className="text-center">
-            <div className="card">
-                <div className="card-body">
-                    <h4>{filmDetails?.title}</h4>
-                    <p>Director: {filmDetails?.director}</p>
-                    <p>Release Date: {filmDetails?.release_date}</p>
-                    <p>Rating: {filmDetails?.rt_score}/100</p>
-                    <Link to="/films">Go Back</Link>
+        <div className="container">
+            <section className="mt-5">
+                <div className="card shadow my2 mb-2" style={{ height: "auto", width: "30rem", margin: "0 auto", float: "none", marginBottom: "10px" }}>
+                    <div className="card-body">
+                        <h4 className="card-title text-center mb-5">{filmDetails?.title}</h4>
+                        <hr />
+                        <p className="card-text">Director: <b>{filmDetails?.director}</b></p>
+                        <hr />
+                        <p className="card-text">Produced By: <b>{filmDetails?.producer}</b></p>
+                        <hr />
+                        <p className="card-text">Release Date: <b>{filmDetails?.release_date}</b></p>
+                        <hr />
+                        <p className="card-text">Rating: <a style={{color: "green"}}>{filmDetails?.rt_score}</a>/100</p>
+                        <hr />
+                        <p className="card-text">{filmDetails?.description}</p>
+                        <Link to="/films" className="btn btn-secondary justify-content-center">Go Back</Link>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
     );
 }
